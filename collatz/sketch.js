028046e518cb80dfd7dec7fff08044257340dca8
Martin Guggisberg <martin.guggisberg@gmail.com>
2016-10-10 22:06:37 +0200

    new font

--- collatz/sketch.js
+++ collatz/sketch.js
@@ -15,11 +15,16 @@ function drawdot(col){
 }
 
 
+
+
 function nextNum(z){
 	var timestep = parseInt(40000/speed.value()/speed.value());
 
 	outzahl.html(z);
-	if (z < 2) return true
+	if (z < 2) {
+		button.show();
+		return true;
+	}
 
 	if (z%2===0){
 		drawdot('#fab125');
@@ -39,14 +44,8 @@ function calc_collatz(){
 	 posX = 0;
 	 posY = 0;
 	 background('#0fbfcf');
+	 button.hide();
      nextNum(zahl);
-
-	// while (zahl > 1){
-	// 	console.log(zahl);
-	// 	outzahl.html(zahl);
-	// 	zahl=nextNum(zahl);
-	// }
-	// outzahl.html(zahl);
 }
 
 
@@ -60,20 +59,28 @@ function setup() {
   input = createInput();
   input.position(20, 65);
   input.value(42);
+  input.style("font-family","Arial");
+  input.style("font-size","32px");
+  input.size(300);
 
   button = createButton('submit');
-  button.position(150, 65);
+  button.position(330, 55);
   button.mousePressed(calc_collatz);
+  button.style("background","#AA9988");
+  button.style("background-image","linear-gradient(to bottom, #AA9988, #888888)");
+  button.style("border-radius","28px");
+  button.style("font-family","Arial");
+  button.style("color","#ffffff");
+  button.style("font-size","32px");
+  button.style("padding","10px 20px 10px 20px");
+  button.style("text-decoration","none");
 
   outzahl = createElement('h2', '');
-  outzahl.position(40, 90);
-  outzahl.style('font-size: 60px; color: white');
+  outzahl.position(90, 100);
+  outzahl.style('font-size: 70px; color: white');
 
   speed = createSlider(4, 48, 15);
   speed.position(30,420);
-
-
-
 
 }
 
